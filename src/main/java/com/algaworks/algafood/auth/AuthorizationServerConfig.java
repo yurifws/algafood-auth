f332e08f56fcb7e97528204963f01a0b7315e754
yurifws <yurifws@gmail.com>
2020-09-24 00:26:59 -0300

    Configurando o Authorization Code Grant Type - Uri solicitante = http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalytics&state=abc&redirect_uri=http://aplicacao-cliente
    Uri de resposta = http://aplicacao-cliente/?code=BUCski&state=abc

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -29,19 +29,25 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 		clients
 			.inMemory()
 				.withClient("algafood-web")
-					.secret(passwordEncoder.encode("web123"))
-					.authorizedGrantTypes("password", "refresh_token")
-					.scopes("write","read")
-					.accessTokenValiditySeconds(60 * 60 * 6) // 6 horas
-					.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
+				.secret(passwordEncoder.encode("web123"))
+				.authorizedGrantTypes("password", "refresh_token")
+				.scopes("write","read")
+				.accessTokenValiditySeconds(60 * 60 * 6) // 6 horas
+				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
 			.and()
 				.withClient("faturamento")
-					.secret(passwordEncoder.encode("faturamento123"))
-					.authorizedGrantTypes("client_credentials")
-					.scopes("write","read")
+				.secret(passwordEncoder.encode("faturamento123"))
+				.authorizedGrantTypes("client_credentials")
+				.scopes("write","read")
+			.and()
+				.withClient("foodanalytics")
+				.secret(passwordEncoder.encode("food123"))
+				.authorizedGrantTypes("authorization_code")
+				.scopes("write","read")
+				.redirectUris("http://aplicacao-cliente")
 			.and()
 				.withClient("checktoken")
-					.secret(passwordEncoder.encode("check123")); 
+				.secret(passwordEncoder.encode("check123")); 
 	}
 	
 	@Override
