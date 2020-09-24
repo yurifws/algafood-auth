52e1a2b9e7077eb5dfd2c3461f8d2787c2ca87bd
yurifws <yurifws@gmail.com>
2020-09-23 23:04:56 -0300

    Configurando o Refresh Token Grant Type no Authorization Server

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,15 +21,18 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients
 			.inMemory()
 				.withClient("algafood-web")
 					.secret(passwordEncoder.encode("web123"))
-					.authorizedGrantTypes("password")
+					.authorizedGrantTypes("password", "refresh_token")
 					.scopes("write","read")
-					.accessTokenValiditySeconds(60*60*6) // 6 horas (padrao 12h)
+					.accessTokenValiditySeconds(15)
 				.and()
 				.withClient("checktoken")
 					.secret(passwordEncoder.encode("check123")); 
@@ -42,7 +46,9 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDetailsService);
 	}
 
 }
