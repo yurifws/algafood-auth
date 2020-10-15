8c3b1df633cc4bf50970365d21ac81e46e899c57
yurifws <yurifws@gmail.com>
2020-10-15 16:45:18 -0300

    Revisando o fluxo de aprovação do Authorization Code com JWT

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
@@ -89,7 +92,14 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 			.reuseRefreshTokens(false)
 			//.tokenStore(redisTokenStore())
 			.accessTokenConverter(jwtAccessTokenConverter())
+			.approvalStore(approvalStore(endpoints.getTokenStore())) // deve ser apos o accessTokenConverter
 			.tokenGranter(tokenGranter(endpoints));
+	}
+	
+	private ApprovalStore approvalStore(TokenStore tokenStore) {
+		var approvalStore = new TokenApprovalStore();
+		approvalStore.setTokenStore(tokenStore);
+		return approvalStore;
 	}
 	
 	//private TokenStore redisTokenStore() {
